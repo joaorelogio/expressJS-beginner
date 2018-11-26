e3d09da562837c97d5ca67f9307e3a262c07d210
João Paulo Relógio <joaoprelogio@gmail.com>
2018-11-26 00:45:58 +0000

    Add error msg

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Global Varibles
 app.use(function(req, res, next){
   res.locals.errors = null;
+  next();
 });
 
 // Express Validator Middleware
